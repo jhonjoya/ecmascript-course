76a3009f99c4f9533fc673ac41d79625967c4fe7
Jhon Joya <jhonjoya@gmail.com>
2022-04-07 19:05:49 -0500

    Generators added

--- src/es6/index.js
+++ src/es6/index.js
@@ -155,4 +155,13 @@ function* helloworld() {
     if(true){
         yield 'Hello, ';
     }
+    if (true) {
+        yield 'World';
+    }
 };
+
+const generatorHello = helloworld();
+
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
